801dcd48b8a2acac9f6b2ad0e8a250792026728b
ElaFloBiz <67695537+ElaFloBiz@users.noreply.github.com>
2026-02-08 23:12:04 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "buget-local-v1";
+const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) =>
+      cached ||
+      fetch(e.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+        return resp;
+      }).catch(() => cached)
+    )
+  );
+});
